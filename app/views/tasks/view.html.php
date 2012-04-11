f0e638817ce2f8a45933fc9ab052ca984990a1ab
Štěpán Pilař <xpils00@vse.cz>
2012-04-11 20:29:57 +0200

    Added CSS for Lists::view

--- app/views/tasks/view.html.php
+++ app/views/tasks/view.html.php
@@ -1,10 +1,14 @@
 <?php
-	$class = 'task';
+	$class = 'task contain';
 	if ($task->completed) {
 		$class .= ' completed';
 	}
 ?>
 	<li data-task_id="<?php echo $task->id ?>" class="<?php echo $class ?>">
+		<?php
+			echo $this->html->link('✔', array('controller' => 'tasks', 'action' => 'complete', 'id' => $task->id), array('class' => 'complete', 'title' => $t('Complete')));
+			echo $this->html->link('✔', array('controller' => 'tasks', 'action' => 'uncomplete', 'id' => $task->id), array('class' => 'uncomplete', 'title' => $t('Uncomplete')));
+		?>
 		<div class="content">
 			<h2 class="taskName"><?= $task->name ?></h2>
 			<?php if (!empty($task->desc)) {
@@ -12,10 +16,10 @@
 			}
 			?>
 		</div>
-		<?php
-		echo $this->html->link($t('Edit'), array('controller' => 'tasks', 'action' => 'edit', 'id' => $task->id), array('class' => 'edit'));
-		echo $this->html->link($t('Completed'), array('controller' => 'tasks', 'action' => 'complete', 'id' => $task->id), array('class' => 'complete'));
-		echo $this->html->link($t('Uncompleted'), array('controller' => 'tasks', 'action' => 'uncomplete', 'id' => $task->id), array('class' => 'uncomplete'));
-		echo $this->form->formLink($t('Delete'), array('controller' => 'tasks', 'action' => 'delete', 'id' => $task->id), array('class' => 'delete'));
-		?>
+		<div class="links">
+			<?php
+			echo $this->html->link($t('Edit'), array('controller' => 'tasks', 'action' => 'edit', 'id' => $task->id), array('class' => 'edit', 'title' => $t('Edit')));
+			echo $this->form->formLink($t('Delete'), array('controller' => 'tasks', 'action' => 'delete', 'id' => $task->id), array('class' => 'delete', 'title' => $t('Delete')));
+			?>
+		</div>
 	</li>
