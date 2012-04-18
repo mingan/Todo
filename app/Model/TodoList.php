6184994d8bdc83e444f9e57279c58bae8cdbf856
Štěpán Pilař <xpils00@vse.cz>
2012-04-18 12:58:58 +0200

    Added generation of hashes for new lists

--- app/Model/TodoList.php
+++ app/Model/TodoList.php
@@ -14,7 +14,7 @@ class TodoList extends AppModel {
  * @var string
  */
 	public $displayField = 'name';
-	
+
 	//The Associations below have been created with all possible keys, those that are not needed can be removed
 
 /**
@@ -39,6 +39,53 @@ class TodoList extends AppModel {
 	);
 
 	public $actsAs = array('Containable');
+
+	public function beforeSave($options = array()) {
+		parent::beforeSave($options);
+		$options = array_merge(
+			$options,
+			array(
+				'generateHash' => true
+			)
+		);
+
+		if ($options['generateHash']) {
+			$this->data = $this->generateHash($this->data);
+		}
+
+		return true;
+	}
+
+	public function generateHash ($data) {
+		if (!(empty($data['TodoList']['hash'])
+				&& empty($data['TodoList']['public_hash']))) {
+			return $data;
+		}
+
+		do {
+			$test = substr(sha1(rand() . serialize($data) . Configure::read('Security.salt')), 0, 20);
+			$test2 = substr(sha1(serialize($data) . Configure::read('Security.salt') . rand()), 0, 20);
+			$found = $this->find(
+					'count',
+					array(
+						'conditions' => array(
+							'OR' => array(
+								'hash' => $test,
+								'hash' => $test2,
+								'public_hash' => $test,
+								'public_hash' => $test2
+							)
+						),
+						'recursive' => -1
+					)
+			);
+		} while ($found);
+
+		$data['TodoList']['hash'] = $test;
+		$data['TodoList']['public_hash'] = $test2;
+
+		return $data;
+	}
 
 	public function filterTasks ($data) {
 		if (!isset($data['Task'])) {
