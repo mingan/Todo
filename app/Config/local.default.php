a27781d87eed9f2e3b9ae2e84f2ca90e865f7211
Štěpán Pilař <xpils00@vse.cz>
2012-05-02 16:47:57 +0200

    Added proper REAMDE file

---
+++ app/Config/local.default.php
@@ -0,0 +1,8 @@
+<?php
+ $_SERVER["DB1_HOST"] = 'localhost';
+ $_SERVER["DB1_PORT"] = null;
+ $_SERVER["DB1_USER"] = '';
+ $_SERVER["DB1_PASS"] = '';
+ $_SERVER["DB1_NAME"] = '';
+
+ $_SERVER['ENVIRONMENT'] = 'development';
